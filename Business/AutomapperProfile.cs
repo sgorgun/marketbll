b90e45fea55dd38414bbd00d3a2ef756fb42e153
Yuriy Antonov <y.s.antonov.edu@gmail.com>
2023-01-30 14:18:11 +0200

    Initial commit

---
+++ Business/AutomapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Business.Models;
+using Data.Entities;
+using System.Linq;
+
+namespace Business
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Receipt, ReceiptModel>()
+                .ForMember(rm => rm.ReceiptDetailsIds, r => r.MapFrom(x => x.ReceiptDetails.Select(rd => rd.Id)))
+                .ReverseMap();
+
+            //TODO: Create mapping for Product and ProductModel
+
+            //TODO: Create mapping for ReceiptDetail and ReceiptDetailModel
+
+            //TODO: Create mapping that combines Customer and Person into CustomerModel
+
+            //TODO: Create mapping for ProductCategory and ProductCategoryModel
+
+        }
+    }
+}
